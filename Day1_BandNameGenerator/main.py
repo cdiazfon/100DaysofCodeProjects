8e2475de629036573e07f962e03d9dc19ed99ca3
cdiazfon <77707813+cdiazfon@users.noreply.github.com>
2022-01-05 20:33:34 -0800

    Project 1, Day 1: Band Name Generator

---
+++ Day1_BandNameGenerator/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    print(f"This is the Band Name Generator by me")
+    city_name = input('Enter the name of the city that you represent: ')
+    pet_name = input('Enter the name of one of your pets lol: ')
+    print(f"The name of your band is : The {city_name} {pet_name}sT.")
+
+
+if __name__ == '__main__':
+    main()
